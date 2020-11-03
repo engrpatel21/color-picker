3cb0eec26cb00914c896f8bfd5b7f03298297597
engrpatel21 <engrpatel21@gmail.com>
2020-11-03 12:32:26 -0500

    first commit

---
+++ src/Pallete.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+   state = {  }
+   render() { 
+      return ( 
+         <div className="Palette">
+            {/* navBar */}
+            <div className="Palette-colors">
+               {/* color boxes */}
+            </div>
+            {/* footer */}
+         </div>
+       );
+   }
+}
+ 
+export default Palette;
