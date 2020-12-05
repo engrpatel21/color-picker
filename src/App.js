6408bfdb7881fff0e695c739fe282d887c2cee58
engrpatel21 <engrpatel21@gmail.com>
2020-12-05 11:42:17 -0500

    add color helper functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Pallete from "./Pallete/Pallete";
 import "./App.css";
 import seedColors from "./seedColors";
+import generatePalette from './colorHelpers'
 
 function App() {
+  console.log(generatePalette(seedColors[4]))
   return (
     <div >
       <Pallete {...seedColors[5]} />
